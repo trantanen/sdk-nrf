26b9c9997ecfff86c3d367d5437ea4dc07bb60bf
Tommi Rantanen <tommi.rantanen@nordicsemi.no>
2021-02-22 00:42:34 +0200

    tests:lib:sms: 2nd set of tests
    Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

--- tests/lib/sms/src/sms_test.c
+++ tests/lib/sms/src/sms_test.c
@@ -54,19 +54,17 @@ static void sms_callback(struct sms_data *const data, void *context)
 	TEST_ASSERT_EQUAL(test_sms_header.time.minute, sms_header->time.minute);
 	TEST_ASSERT_EQUAL(test_sms_header.time.second, sms_header->time.second);
 
-	TEST_ASSERT_EQUAL_STRING("Moi", sms_header->ud);
-	TEST_ASSERT_EQUAL(3, sms_header->ud_len);
-/*
-	if (sms_header->app_port.present) {
-			sms_header->app_port.dest_port,
-			sms_header->app_port.src_port);
-	}
-	if (sms_header->concatenated.present) {
-			sms_header->concatenated.ref_number,
-			sms_header->concatenated.seq_number,
-			sms_header->concatenated.total_msgs);
-	}
-*/
+	TEST_ASSERT_EQUAL_STRING(test_sms_header.ud, sms_header->ud);
+	TEST_ASSERT_EQUAL(test_sms_header.data_len, sms_header->data_len);
+
+	TEST_ASSERT_EQUAL(test_sms_header.app_port.present, sms_header->app_port.present);
+	TEST_ASSERT_EQUAL(test_sms_header.app_port.dest_port, sms_header->app_port.dest_port);
+	TEST_ASSERT_EQUAL(test_sms_header.app_port.src_port, sms_header->app_port.src_port);
+
+	TEST_ASSERT_EQUAL(test_sms_header.concatenated.present, sms_header->concatenated.present);
+	TEST_ASSERT_EQUAL(test_sms_header.concatenated.ref_number, sms_header->concatenated.ref_number);
+	TEST_ASSERT_EQUAL(test_sms_header.concatenated.seq_number, sms_header->concatenated.seq_number);
+	TEST_ASSERT_EQUAL(test_sms_header.concatenated.total_msgs, sms_header->concatenated.total_msgs);
 }
 
 void sms_init_helper()
@@ -95,25 +93,80 @@ void sms_uninit_helper()
 	sms_uninit();
 }
 
+/********* SMS SEND TESTS ***********************/
+/* The following site used as a reference for generated messages:
+   http://smstools3.kekekasvi.com/topic.php?id=288 */
+
 void test_sms_init_uninit(void)
 {
 	sms_init_helper();
 	sms_uninit_helper();
 }
 
-void test_send(void)
+void test_send_len3_number10plus(void)
 {
 	enum at_cmd_state state = 0;
-	__wrap_at_cmd_write_ExpectAndReturn("AT+CMGS=17\r0031000C915348803061250000FF03CD771A\x1A", NULL, 0, &state, 0);
+	__wrap_at_cmd_write_ExpectAndReturn("AT+CMGS=16\r0031000A9121436587090000FF03CD771A\x1A", NULL, 0, &state, 0);
 	__wrap_at_cmd_write_IgnoreArg_buf();
 	__wrap_at_cmd_write_IgnoreArg_buf_len();
 
-	int ret = sms_send("+358408031652", "Moi");
+	int ret = sms_send("+1234567890", "Moi");
 	TEST_ASSERT_EQUAL(0, ret);
 	TEST_ASSERT_EQUAL(AT_CMD_OK, state);
 }
 
-void test_send_concatenated(void)
+void test_send_len1_number20plus(void)
+{
+	enum at_cmd_state state = 0;
+	__wrap_at_cmd_write_ExpectAndReturn("AT+CMGS=19\r0031001491214365870921436587090000FF0131\x1A", NULL, 0, &state, 0);
+	__wrap_at_cmd_write_IgnoreArg_buf();
+	__wrap_at_cmd_write_IgnoreArg_buf_len();
+
+	int ret = sms_send("+12345678901234567890", "1");
+	TEST_ASSERT_EQUAL(0, ret);
+	TEST_ASSERT_EQUAL(AT_CMD_OK, state);
+}
+
+/* Test message length 7 to see that alignment with 7bit GSM encoding works properly. */
+void test_send_len7_number11(void)
+{
+	enum at_cmd_state state = 0;
+	__wrap_at_cmd_write_ExpectAndReturn("AT+CMGS=21\r0031000B912143658709F10000FF0731D98C56B3DD00\x1A", NULL, 0, &state, 0);
+	__wrap_at_cmd_write_IgnoreArg_buf();
+	__wrap_at_cmd_write_IgnoreArg_buf_len();
+
+	int ret = sms_send("12345678901", "1234567");
+	TEST_ASSERT_EQUAL(0, ret);
+	TEST_ASSERT_EQUAL(AT_CMD_OK, state);
+}
+
+/* Test message length 8 to see that alignment with 7bit GSM encoding works properly. */
+void test_send_len8_number1(void)
+{
+	enum at_cmd_state state = 0;
+	__wrap_at_cmd_write_ExpectAndReturn("AT+CMGS=16\r0031000191F10000FF0831D98C56B3DD70\x1A", NULL, 0, &state, 0);
+	__wrap_at_cmd_write_IgnoreArg_buf();
+	__wrap_at_cmd_write_IgnoreArg_buf_len();
+
+	int ret = sms_send("1", "12345678");
+	TEST_ASSERT_EQUAL(0, ret);
+	TEST_ASSERT_EQUAL(AT_CMD_OK, state);
+}
+
+/* Test message length 9 to see that alignment with 7bit GSM encoding works properly. */
+void test_send_len9_number5(void)
+{
+	enum at_cmd_state state = 0;
+	__wrap_at_cmd_write_ExpectAndReturn("AT+CMGS=19\r00310005912143F50000FF0931D98C56B3DD7039\x1A", NULL, 0, &state, 0);
+	__wrap_at_cmd_write_IgnoreArg_buf();
+	__wrap_at_cmd_write_IgnoreArg_buf_len();
+
+	int ret = sms_send("12345", "123456789");
+	TEST_ASSERT_EQUAL(0, ret);
+	TEST_ASSERT_EQUAL(AT_CMD_OK, state);
+}
+
+void test_send_concat_220(void)
 {
 	enum at_cmd_state state1 = 0;
 	__wrap_at_cmd_write_ExpectAndReturn("AT+CMGS=153\r0061010C915348803061250000A005000301020162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966B49AED86CBC162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966B49AED86CBC162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966B49AED86CBC162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966\x1A", NULL, 0, &state1, 0);
@@ -131,7 +184,7 @@ void test_send_concatenated(void)
 	TEST_ASSERT_EQUAL(AT_CMD_OK, state2);
 }
 
-void test_send_concatenated2(void)
+void test_send_concat_291(void)
 {
 	enum at_cmd_state state1 = 0;
 	__wrap_at_cmd_write_ExpectAndReturn("AT+CMGS=153\r0061030C915348803061250000A005000302020162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966B49AED86CBC162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966B49AED86CBC162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966B49AED86CBC162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966\x1A", NULL, 0, &state1, 0);
@@ -149,14 +202,16 @@ void test_send_concatenated2(void)
 	TEST_ASSERT_EQUAL(AT_CMD_OK, state2);
 }
 
-void test_recv(void)
+/********* SMS SEND TESTS ***********************/
+
+void test_recv_len3_number10(void)
 {
 	sms_init_helper();
 
-	strcpy(test_sms_data.alpha, "+358408031652");
+	strcpy(test_sms_data.alpha, "+1234567890");
 	test_sms_data.length = 22;
-	strcpy(test_sms_data.pdu, "0791534874894320040C9153488030612500001220900285438003CD771A");
-	test_sms_header.ud_len = 3;
+	strcpy(test_sms_data.pdu, "0791534874894320040A91214365870900001220900285438003CD771A");
+	test_sms_header.data_len = 3;
 	strcpy(test_sms_header.ud, "Moi");
 	test_sms_header.time.year = 21;
 	test_sms_header.time.month = 2;
@@ -166,7 +221,72 @@ void test_recv(void)
 	test_sms_header.time.second = 34;
 
 	__wrap_at_cmd_write_ExpectAndReturn("AT+CNMA=1", NULL, 0, NULL, 0);
-	sms_at_handler(NULL, "+CMT: \"+358408031652\",22\r\n0791534874894320040C9153488030612500001220900285438003CD771A\r\n");
+	sms_at_handler(NULL, "+CMT: \"+1234567890\",22\r\n0791534874894320040A91214365870900001220900285438003CD771A\r\n");
+
+	sms_uninit_helper();
+}
+
+void test_recv_concat(void)
+{
+	sms_init_helper();
+
+	strcpy(test_sms_data.alpha, "+1234567890");
+	test_sms_data.length = 22;
+	strcpy(test_sms_data.pdu, "0791534874894310440A912143658709000012201232054480A00500037E020162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966B49AED86CBC162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966B49AED86CBC162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966B49AED86CBC162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966");
+	test_sms_header.data_len = 153;
+	strcpy(test_sms_header.ud, "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123");
+	test_sms_header.time.year = 21;
+	test_sms_header.time.month = 2;
+	test_sms_header.time.day = 21;
+	test_sms_header.time.hour = 23;
+	test_sms_header.time.minute = 50;
+	test_sms_header.time.second = 44;
+
+	test_sms_header.concatenated.present = true;
+	test_sms_header.concatenated.total_msgs = 2;
+	test_sms_header.concatenated.ref_number = 126;
+	test_sms_header.concatenated.seq_number = 1;
+
+	__wrap_at_cmd_write_ExpectAndReturn("AT+CNMA=1", NULL, 0, NULL, 0);
+	sms_at_handler(NULL, "+CMT: \"+1234567890\",22\r\n0791534874894310440A912143658709000012201232054480A00500037E020162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966B49AED86CBC162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966B49AED86CBC162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966B49AED86CBC162B219AD66BBE172B0986C46ABD96EB81C2C269BD16AB61B2E078BC966\r\n");
+
+	strcpy(test_sms_data.pdu, "0791534874894320440A912143658709000012201232054480910500037E02026835DB0D9783C564335ACD76C3E56031D98C56B3DD7039584C36A3D56C375C0E1693CD6835DB0D9783C564335ACD76C3E56031D98C56B3DD7039584C36A3D56C375C0E1693CD6835DB0D9783C564335ACD76C3E56031D98C56B3DD7039584C36A3D56C375C0E1693CD6835DB0D9783C564335ACD76C3E56031");
+	test_sms_header.data_len = 138;
+	strcpy(test_sms_header.ud, "456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901");
+	test_sms_header.concatenated.seq_number = 2;
+
+	__wrap_at_cmd_write_ExpectAndReturn("AT+CNMA=1", NULL, 0, NULL, 0);
+	sms_at_handler(NULL, "+CMT: \"+1234567890\",22\r\n0791534874894320440A912143658709000012201232054480910500037E02026835DB0D9783C564335ACD76C3E56031D98C56B3DD7039584C36A3D56C375C0E1693CD6835DB0D9783C564335ACD76C3E56031D98C56B3DD7039584C36A3D56C375C0E1693CD6835DB0D9783C564335ACD76C3E56031D98C56B3DD7039584C36A3D56C375C0E1693CD6835DB0D9783C564335ACD76C3E56031\r\n");
+
+	sms_uninit_helper();
+}
+
+void test_recv_wap_push(void)
+{
+	sms_init_helper();
+
+	strcpy(test_sms_data.alpha, "69923221");
+	test_sms_data.length = 22;
+	strcpy(test_sms_data.pdu, "004408819629231200040230503231732b1c0b05040b84000000037c0101010603c4af9a40027d76b13101320138");
+	test_sms_header.data_len = 16;
+	strcpy(test_sms_header.ud, "\x01\x06\x03\xC4\xAF\x9A\x40\x02\x7D\x76\xB1\x31\x01\x32\x01\x38");//"\x01\x06\x03\xC4\xAF\x9A@\x02}v\xB11\x012\x018");//00037C0101010603C4AF9A40027D76B13101320138");
+	test_sms_header.time.year = 20;
+	test_sms_header.time.month = 3;
+	test_sms_header.time.day = 5;
+	test_sms_header.time.hour = 23;
+	test_sms_header.time.minute = 13;
+	test_sms_header.time.second = 37;
+
+	test_sms_header.app_port.present = true;
+	test_sms_header.app_port.dest_port = 2948;
+
+	test_sms_header.concatenated.present = true;
+	test_sms_header.concatenated.total_msgs = 1;
+	test_sms_header.concatenated.ref_number = 124;
+	test_sms_header.concatenated.seq_number = 1;
+
+	__wrap_at_cmd_write_ExpectAndReturn("AT+CNMA=1", NULL, 0, NULL, 0);
+	sms_at_handler(NULL, "+CMT: \"69923221\",22\r\n004408819629231200040230503231732b1c0b05040b84000000037c0101010603c4af9a40027d76b13101320138\r\n");
 
 	sms_uninit_helper();
 }
